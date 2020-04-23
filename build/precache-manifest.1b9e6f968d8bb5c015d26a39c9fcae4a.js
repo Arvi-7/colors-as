66c8324212dd13157c3310d0e2c4d6e795a6446c
Arvi-7 <palisettyaravind69@gmail.com>
2020-04-23 16:02:38 +0530

    firstcommit

---
+++ build/precache-manifest.1b9e6f968d8bb5c015d26a39c9fcae4a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34399475dbdfb6eaf2457b455332456e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "989717281df4f46a7c83",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "39fe797f1ea6758cbf98",
+    "url": "/static/css/main.8544c63f.chunk.css"
+  },
+  {
+    "revision": "989717281df4f46a7c83",
+    "url": "/static/js/2.d5a27340.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d5a27340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fe797f1ea6758cbf98",
+    "url": "/static/js/main.1a6bc23d.chunk.js"
+  },
+  {
+    "revision": "426b1cd6c048807e0c5b",
+    "url": "/static/js/runtime-main.e45c20b8.js"
+  },
+  {
+    "revision": "d465a3a90ca2ac9868d147e0d9c2133f",
+    "url": "/static/media/a1.d465a3a9.png"
+  },
+  {
+    "revision": "5963b1aac64596ff775d544a88dca69d",
+    "url": "/static/media/img 4.5963b1aa.png"
+  },
+  {
+    "revision": "ef3422b1cbd87100af653a7af54c89d2",
+    "url": "/static/media/img 5.ef3422b1.png"
+  },
+  {
+    "revision": "b32941400ea3ba9c8b91ddd84dc97c19",
+    "url": "/static/media/img.b3294140.png"
+  },
+  {
+    "revision": "4d33acdeb1ea5716ba038a24a0ac4c15",
+    "url": "/static/media/img1.4d33acde.png"
+  },
+  {
+    "revision": "92ffc07ef59f820eed389f3aa97c9236",
+    "url": "/static/media/img2.92ffc07e.png"
+  },
+  {
+    "revision": "b05bb4dafc1531c364242a9ba2e780f2",
+    "url": "/static/media/img3.b05bb4da.png"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import ReactDom from 'react-dom'
+import Button from 'react-dom';
+import useState from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav,Navbar} from 'react-bootstrap';
+import { FaBeer, FaGoogle, FaGooglePlus,FaInstagram, FaYoutube } from 'react-icons/fa';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/container';
+import a1 from './a1.png';
+import Alert from 'react-bootstrap/Alert';
+import img from './img.png';
+import img1 from './img1.png'
+import img2 from './img2.png'
+import img3 from './img3.png'
+import img4 from './img 4.png'
+import img5 from './img 5.png'
+
+import Popover from 'react-bootstrap/Popover'
+import {OverlayTrigger,overlay} from 'react-bootstrap'
+import render from 'react'
+import StickyFooter from 'react-sticky-footer'
+import Figure from 'react-bootstrap/Figure'
+
+
+import {ToastHeader,Toast,Toastbody} from 'react-bootstrap';
+
+
+
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Title as="h3">Popover right</Popover.Title>
+    <Popover.Content>
+      And here's some <strong>amazing</strong> content. It's very engaging.
+      right?
+    </Popover.Content>
+  </Popover>
+);
+
+
+
+const example = () =>(
+  <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+  <Button variant="success">Click me to see</Button>
+</OverlayTrigger>
+)
+
+
+
+
+function App() {
+  
+  
+  return (
+    
+    <>
+    <div className="root col" >
+     
+      <Navbar sticky='top' className='navbar' collapseOnSelect expand="lg"  variant="dark">
+  <Navbar.Brand href="#home">colors</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Home</Nav.Link>
+
+      <Nav.Link eventKey={2} href="#memes">
+        About
+      </Nav.Link>
+      <Nav.Link href="#deets">More</Nav.Link>
+      
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
+<body>
+<Jumbotron  className='jum'>
+  <Container>
+    <img  className='img' src={a1} alt='src'></img>
+    <h1 className='hh'>I'm Arvind</h1>
+    
+  </Container>
+</Jumbotron>
+<hr />
+
+<div id='c'>
+     <div className="row">
+        <div className='col-sm-1'></div>
+          <div id='c1' className="card col-sm-3">
+           <button className='b' onClick={popover}><img  id='i' src={img} alt={img}/></button>
+            
+          </div>
+          <div className='col-sm-4'></div>
+          <div id='c1' className="card col-sm-3">
+              <img id='i' src={img1} alt={img1}/>
+          </div>
+          <div className='col-sm-1'></div>
+
+       </div>
+</div>
+
+<hr />
+
+<div id='c'>
+     <div className="row">
+        <div className='col-sm-1'></div>
+          <div id='c1' className="card col-sm-3">
+              <img   id='i'src={img2} alt={img2}/>
+          </div>
+          <div className='col-sm-4'></div>
+          <div id='c1' className="card col-sm-3">
+              <img id='i'  src={img3} alt={img3}/>
+          </div>
+          <div className='col-sm-1'></div>
+
+       </div>
+</div>
+
+<hr/>
+
+<div id='c'>
+     <div className="row">
+        <div className='col-sm-1'></div>
+          <div id='c1' className="card col-sm-3">
+              <img   id='i' src={img4} alt="Avatar"/>
+          </div>
+          <div className='col-sm-4'></div>
+          <div id='c1' className="card col-sm-3">
+              <img id='i'  src={img5} alt="Avatar"/>
+          </div>
+          <div className='col-sm-1'></div>
+
+       </div>
+
+       <hr />
+       <Jumbotron  className='jum'fluid>
+  <Container>
+    <button className='b' href='https://www.youtube.com/results?search_query=painting+of+flowers+by+bannu'>
+      <FaYoutube  color='black' size={200}/>
+      <h1>hgjckuy</h1>
+  
+</button>
+  </Container>
+</Jumbotron>
+</div>
+
+</body>
+
+
+
+    
+    
+      
+
+</div>
+<div>
+<footer className="page">
+<a href='https://www.instagram.com/sireesha_kosuru/'className='a'> <FaInstagram  color="black" size={50} title='instagram' /></a>
+
+<div  class="footer-copyright text-center py-3"><strong>© 2020 Copyrights Colors</strong>
+<li><small>This is a page where you can explore some colors</small></li> 
+  
+</div>
+
+</footer>
+
+</div>
+
+
+</>
+
+  );
+}
+
+
+export default App;
+
